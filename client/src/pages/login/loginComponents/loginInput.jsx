1d6f3c5bfb232f38f3b49be82bedb4cc3ea650b4
Kevin Rodriguez <krodrigueztruji2962@eagle.fgcu.edu>
2023-09-27 16:42:37 -0400

    begin login functionality

--- client/src/pages/login/loginComponents/loginInput.jsx
+++ client/src/pages/login/loginComponents/loginInput.jsx
@@ -3,6 +3,47 @@ import './components.css';
 import { useNavigate } from 'react-router-dom';
 
 export const LoginInput = () => {
+
+  const [inputs, setInputs] = React.useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {// updating state of inputs on change of input
+    setInputs((inputs) => ({ ...inputs, [e.target.name]: e.target.value }));
+  }
+
+  const navigate = useNavigate();
+
+  const instance = axios.create({
+    baseURL: 'http://localhost:8080',
+    withCredentials: true,
+  });
+  const login = async (email, password) => {
+    const res = await instance.post('/login', {
+      email,
+      password,
+    });
+    if (!res.data){
+      return "User not found"
+    } 
+    return res.data; 
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try{
+      const res = login(inputs.email, inputs.password);
+      if (res === "User not found"){
+        console.log(res);
+      } else {
+        navigate('/dashboard');
+      }
+    } catch (err){
+      console.log(err);
+    }
+  }
+
   
   return (
     <form className='password-input' 
@@ -11,20 +52,22 @@ export const LoginInput = () => {
       <div className="login-title">Login</div>
       <div className="form-control">
         <input 
-          className="email-input" 
+          className="email-input"
+          name="email" 
           type="text" 
           placeholder="Email"
           required
-          // onChange={handleChange} 
+          onChange={handleChange} 
         />
       </div>
       <div className="form-control">
         <input
           className="password-input"
+          name="password"
           type="password"
           placeholder="Password"
           required
-          // onChange={handleChange}
+          onChange={handleChange}
         />
         <small>Error Message</small>
       </div>
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import controller from '../controllers/User.js';
-import e from 'express';
 
 const router = express.Router();
 
